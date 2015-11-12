2c3ef86e52449530cdbf0e338a9866232d6df748
pioneer903 <pioneer903@gmail.com>
2015-11-12 10:01:54 -0500

    added project files
    Signed-off-by: pioneer903 <pioneer903@gmail.com>

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name creativedatesApp
+ * @description
+ * # creativedatesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('creativedatesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
