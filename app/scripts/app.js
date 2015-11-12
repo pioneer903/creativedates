270f5e8fee8b2e7518c9b1600f9875c8aea5de6a
Робота <job@MacBook-Pro-Vitalii.local>
2015-11-12 20:18:29 +0200

    added new files
    Signed-off-by: Робота <job@MacBook-Pro-Vitalii.local>

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name creativedatesApp
+ * @description
+ * # creativedatesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('creativedatesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name creativedatesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the creativedatesApp
+ */
+angular.module('creativedatesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
