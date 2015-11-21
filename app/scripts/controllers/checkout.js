31b63722d375dc61a2b11cd9992b6539ac7bdc46
Робота <job@MacBook-Pro-Vitalii.local>
2015-11-21 21:02:15 +0200

    added checkout page
    Signed-off-by: Робота <job@MacBook-Pro-Vitalii.local>

---
+++ app/scripts/controllers/checkout.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+/**
+ * @ngdoc function
+ * @name creativedatesApp.controller:CheckoutCtrl
+ * @description
+ * # CheckoutCtrl
+ * Controller of the creativedatesApp
+ */
+angular.module('creativedatesApp')
+  .controller('CheckoutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+})();
